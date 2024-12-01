5c12bd1f6530ffb60013c4703d9e6b610b861add
Kenneth Bacaltos <kennethbacaltos091@gmail.com>
2024-12-01 13:11:07 +0800

    Fixed design

--- src/components/play.tsx
+++ src/components/play.tsx
@@ -4,6 +4,7 @@ import rock from "../assets/images/icon-rock.svg";
 import paper from "../assets/images/icon-paper.svg";
 import lizard from "../assets/images/icon-lizard.svg";
 import spock from "../assets/images/icon-spock.svg";
+import Pulse from "./pulse";
 
 type PlayProps = {
   playerChoice: TChoice | null
@@ -38,16 +39,20 @@ function Play({ playerChoice, computerChoice, nextRound, setNextRound, result, s
         <h1>You Picked</h1>
         <div className={`play-player-picked-${playerChoice!}`}>
           <img  src={choice[playerChoice!]} alt={`player picked ${playerChoice}`}/>
+          <div className="shadow-result"></div>
+          {result === "win" && <Pulse />}
         </div>
       </div>
       {result !== undefined && <div className="play-result-container">
-        <h1>YOU {result}</h1>
+        {result !== "draw" ? <h1>YOU {result}</h1> : <h1>{result}</h1>}
         <button onClick={() => { setNextRound(!nextRound); setPlayerChoice(null); }}>Play again</button>
       </div>}
       <div className="play-computer-container">
         <h1>The House Picked</h1>
         <div className={`play-computer-picked-${computerChoice!}`}>
           <img  src={choice[computerChoice!]} alt={`computer picked ${computerChoice}`}/>
+          <div className="shadow-result"></div>
+          {result === "lose" && <Pulse />}
         </div>
       </div>
     </div>
---
+++ src/components/pulse.tsx
@@ -0,0 +1,13 @@
+function Pulse() {
+    return (
+        <div className="pulse-main-container">
+            <div className="pulse">
+                <span className="pulse-1"></span>
+                <span className="pulse-2"></span>
+                <span className="pulse-3"></span>
+            </div>
+        </div>
+    )
+}
+
+export default Pulse
