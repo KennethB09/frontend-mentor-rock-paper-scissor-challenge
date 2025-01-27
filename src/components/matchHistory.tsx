94e47f8142b3ba4bb8508764a416ef178bafe89d
Kenneth Bacaltos <kennethbacaltos091@gmail.com>
2025-01-27 14:49:27 +0800

    Fixed Design

--- src/components/matchHistory.tsx
+++ src/components/matchHistory.tsx
@@ -9,17 +9,18 @@ type MatchHistoryProps = {
 function MatchHistory({ history, toggleHistory }: MatchHistoryProps) {
   const date = new Date()
   return (
-    <div className="match-history-main-container">
-      <div className="match-history-title-close-container">
-        <h1>Match History</h1>
-        <button onClick={toggleHistory}><img src={x} alt="close"/></button>
+    <div className="bg-clickable" onClick={toggleHistory}>
+      <div className="match-history-main-container">
+        <div className="match-history-title-close-container">
+          <h1>Match History</h1>
+          <button onClick={toggleHistory}><img src={x} alt="close" /></button>
+        </div>
+        {history.length !== 0 ? <ul>
+          {history.map((h, i) => {
+            return <li key={i}><span className="match-history-result">{h.result}</span><p>You {h.player}<strong>VS</strong>Computer {h.computer}</p><span className="match-history-date">{date.toLocaleDateString()}</span></li>
+          })}
+        </ul> : <p>No Match History</p>}
       </div>
-      {history.length !== 0 ? <ul>
-        {history.map((h, i) => {
-          return <li key={i}><span className="match-history-result">{h.result}</span><p>You {h.player}<strong>VS</strong>Computer {h.computer}</p><span className="match-history-date">{date.toLocaleDateString()}</span></li>
-        })}
-      </ul> : <p>No Match History</p>}
-
     </div>
   )
 }
--- src/components/rules.tsx
+++ src/components/rules.tsx
@@ -9,13 +9,15 @@ type rulesProps = {
 function Rules({ toggleRules }: rulesProps) {
 
   return (
-    <div className="rules-main-container">
-      <div className="rules-title-close-container">
-        <h1>rules</h1>
-      </div>
-      <button onClick={toggleRules}><img src={x} alt="close" /></button>
-      <div className="rules-image-container">
-        <img src={rules} alt="rules" />
+    <div className="bg-clickable" onClick={toggleRules}>
+      <div className="rules-main-container">
+        <div className="rules-title-close-container">
+          <h1>rules</h1>
+        </div>
+        <button onClick={toggleRules}><img src={x} alt="close" /></button>
+        <div className="rules-image-container">
+          <img src={rules} alt="rules" />
+        </div>
       </div>
     </div>
   )
